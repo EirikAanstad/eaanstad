92b7f7589f02a4682cbf73ef759cac887652546c
Eirik Aanstad <eaanstad@csumb.edu>
2017-09-09 20:30:06 +0000

    lab2

---
+++ labs/lab2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <meta charset = "utf-8" />
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+    </head>
+    <body>
+        
+        <div id='main'>
+            <?php
+                include 'inc/functions.php';
+                play();
+            ?>
+            <form>
+                <input type="submit" name="spin"/>
+            </form>
+        </div> 
+    </body>
+</html>
