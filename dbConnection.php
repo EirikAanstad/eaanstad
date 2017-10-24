f22847c8a7829d2a5988073798f916f2c677ba9d
Eirik Aanstad <eaanstad@csumb.edu>
2017-10-24 18:39:46 +0000

    skjera

--- dbConnection.php
+++ dbConnection.php
@@ -6,7 +6,7 @@ function getDatabaseConnection() {
     $username = 'root';
     $password = '';
     
-        //when connecting from Heroku
+    //when connecting from Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
